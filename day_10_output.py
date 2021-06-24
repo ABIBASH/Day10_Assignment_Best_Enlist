aa39d5e39900c9392ee8107bd15175929eeb4fd3
ABIBASH <85815699+ABIBASH@users.noreply.github.com>
2021-06-24 22:50:03 +0530

    Add files via upload

---
+++ day_10_output.py
@@ -0,0 +1,26 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Abibash/Documents/Best_enlist_assignments/day10_assignment_best_enlist.py
+Enter amount to be deposit:50000
+
+ Amount deposited: 50000.0 Rupees
+Net Available Balance= 50000.0 Rupees
+
+Thank you for using our bank service
+Enter amount to be Withdrawn:
+= RESTART: C:/Users/Abibash/Documents/Best_enlist_assignments/day10_assignment_best_enlist.py
+Enter amount to be deposit:50000
+
+ Amount deposited: 50000.0 Rupees
+Net Available Balance= 50000.0 Rupees
+
+Thank you for using our bank service
+
+Enter amount to be Withdrawn:6000
+
+ Enter amount to be withdrawn : 6000.0 Rupees
+Net Available Balance= 44000.0 Rupees
+
+Thank you for using our bank service
+>>> 
